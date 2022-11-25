f062816eb7e0697d57a83357e5efe9ada14833fb
Momchil Tsekov <mtsekov@zohomail.eu>
2022-11-25 17:24:57 +0200

    Update

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 // Map global promises
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 
 // Mongoose connect
-let endPoint = ''; //Add endpoint to connect to MLab
-mongoose.connect(endPoint)
-.then(() => console.log('MongoDB Connected...'))
-.catch((err) => console.log(err));
+let endPoint = '' //Add endpoint to connect to MLab
+mongoose
+  .connect(endPoint, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err))
